c2dd3ce66dd3d92a6fe60320c08a1e6b4fe4bafd
VolpinAndrea <andvolpin19@gmail.com>
2022-12-12 16:30:53 +0100

    Legge qualcosa da correggere

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Indirizzo
+    {
+        // ATTRIBUTI Name,Surname,Street,City,Province,ZIP
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private string zip;
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetSurname()
+        {
+            return surname;
+        }
+        public string GetStreet()
+        {
+            return street;
+        }
+        public string GetCity()
+        {
+            return city;
+        }
+        public string GetProvince()
+        {
+            return province;
+        }
+        public string GetZip()
+        {
+            return zip;
+        }
+
+
+        public override string ToString()
+        {
+            return "Nome: " + this.name + "\tCognome: " + this.surname + "\tVia: " + this.street + "Città: " + this.city + "\tProvincia: " + this.province + "\tZIP: " + this.zip;
+        }
+
+
+    }
+}
+
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ListaIndirizzi;
+using System.IO;
+using System.Xml.Linq;
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+//   C:\\Utenti\\Utente\\Desktop\\ExGitVSCode\\ListaIndirizzi\\Indirizzi.txt
+
+
+StreamReader file = File.OpenText("C:\\Users\\Utente\\Desktop\\ExGitVSCode\\ListaIndirizzi\\Indirizzi.txt");
+
+file.WriteLines("");
+file.Close();
+
+
+
+while (!file.EndOfStream)
+    {
+        string riga = file.ReadLine();
+
+
+        string[] informazioniBrano = riga.Split(",");
+         
+        string name = informazioniBrano[0];
+        string surname = informazioniBrano[1];
+        string street = informazioniBrano[2];
+        string city = informazioniBrano[3];
+        string province = informazioniBrano[4];
+        string zip = informazioniBrano[5];
+
+
+        Indirizzo branoEstratto = new Indirizzo(name, surname, street, city, province, zip);
+        listaIndirizzi.Add(branoEstratto);
+        // Console.WriteLine(riga);
+    }
+
+    file.Close();
+
+
+
+foreach (Indirizzo brano in listaIndirizzi)
+{
+    Console.WriteLine(brano);
+}
